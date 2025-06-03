10e3986b5e888271fe8cc4ce2af02c38ac6a6610
s4043579-ps1nov <152124926+s4043579-ps1nov@users.noreply.github.com>
2025-06-03 14:57:37 +1000

    add person implementation

--- Person.java
+++ Person.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 import java.util.Date;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 
-public class person {
+public class Person { // Changed from 'person' to 'Person'
 
     private String personID;
     private String firstName;
@@ -12,7 +15,8 @@ public class person {
     private boolean isSuspended;
     private static final String DATA_FILE_NAME = "persons.txt";
     
- public Person(String personID, String firstName, String lastName, String address, String birthdate) {
+ // Constructor
+    public Person(String personID, String firstName, String lastName, String address, String birthdate) {
         this.personID = personID;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -21,8 +25,8 @@ public class person {
         this.demeritPoints = new HashMap<>(); 
         this.isSuspended = false; 
     }
-
-    // Getters for the private variables
+ // Getters 
+   
     public String getPersonID() {
          return personID;
          }
@@ -44,15 +48,70 @@ public class person {
     public boolean isSuspended() {
         return isSuspended;
     }
+    private boolean isValidPersonID(String user_id){
+        if (user_id.length() != 10){
+            System.out.println("Person ID must be exactly 10 characters long.");
 
+            return false;
+        }
+        char firstChar = user_id.charAt(0);
+        char secondChar = user_id.charAt(1);
+        if (firstChar < '2' || firstChar > '9' || secondChar < '2' || secondChar > '9') {
+            System.out.println("The first two characters of the Person ID must be numbers between 2 and 9.");
+            return false;
+        }
+        int specialCharCount = 0;
+        for (int i = 2; i < 8; i++) {
+            char c = user_id.charAt(i);
+            if (!Character.isLetterOrDigit(c)) {
+                specialCharCount++;
+            }
+        }
+        if (specialCharCount < 2) {
+            System.out.println("There should be at least two special characters between characters 3 and 8.");
+            return false;
+        }
+        char lastChar = user_id.charAt(8);
+        char secondLastChar = user_id.charAt(9);
+        if (!Character.isUpperCase(lastChar) || !Character.isUpperCase(secondLastChar)) {
+            System.out.println("The last two characters of the Person ID must be upper case letters (A – Z).");
+            return false;
+        }
+        return true;
+ }
+    private boolean isValidAddress(String address) {
+        String[] parts = address.split("\\|");
+        if (parts.length != 5) {
+            System.out.println("Address must follow the format: Street Number|Street|City|State|Country.");
+            return false;
+        }
+        if (!parts[3].equalsIgnoreCase("Victoria")) {
+            System.out.println("The State must be Victoria.");
+            return false;
+        }
+        return true;
+    }
+    private boolean isValidBirthdate(String birthdate) {
+        String[] parts = birthdate.split("-");
+        if (parts.length != 3) {
+            System.out.println("Birthdate must follow the format: DD-MM-YYYY.");
+            return false;
+        }
+        if (parts[0].length() != 2 || parts[1].length() != 2 || parts[2].length() != 4) {
+            System.out.println("Birthdate must be in the format DD-MM-YYYY.");
+            return false;
+        }
+        return true;
+    }
 
-    public void addPerson() {
- 
+    public boolean addPerson() {
+          
         //TODO: This method adds information about a person to a TXT file.
 
         //Condition 1: personID should be exactly 10 characters long;
         //the first two characters should be numbers between 2 and 9, there should be at least two special characters between characters 3 and 8,
         //and the last two characters should be upper case letters (A – Z). Example: “56s_d%&fAB”
+       
 
         //Condition 2: The address of the Person should follow the following format: Street Number|Street|City|State|Country.
         //The State should be only Victoria. Example: 32|Highland Street|Melbourne|Victoria|Australia.
@@ -62,7 +121,31 @@ public class person {
         //Instruction: If the Person's information meets the above conditions and any other conditions you may want to consider,
         //the information should be inserted into a TXT file, and the addPerson function should return true.
         //Otherwise, the information should not be inserted into the TXT file, and the addPerson function should return false.
+        if (!isValidPersonID(this.personID)) {
+            System.out.println("Add person failed: Invalid PersonID for '" + this.personID + "'.");
+            return false;
+        }
+        if (!isValidAddress(this.address)) {
+            System.out.println("Add person failed: Invalid Address for '" + this.personID + "'. Address: '" + this.address + "'.");
+            return false;
+        }
+        if (!isValidBirthdate(this.birthdate)) {
+            System.out.println("Add person failed: Invalid Birthdate for '" + this.personID + "'. Birthdate: '" + this.birthdate + "'.");
+            return false;
+        }
 
+        try (FileWriter fw = new FileWriter(DATA_FILE_NAME, true); // 'true' to append to file if it exists
+             PrintWriter pw = new PrintWriter(fw)) {
+
+           
+            String record = String.join(", ", this.personID, this.firstName, this.lastName, this.address, this.birthdate);
+            pw.println(record); 
+            System.out.println("Person added successfully to " + DATA_FILE_NAME + ": ID " + this.personID);
+            return true;
+        } catch (IOException e) {
+            System.err.println("Error writing person data to file '" + DATA_FILE_NAME + "': " + e.getMessage());
+            return false;
+        }
     }
 
     public boolean updatePersonalDetails() {
@@ -100,5 +183,14 @@ public class person {
         //and the addDemeritPoints function should return “Success”. Otherwise, the addDemeritPoints function should return “Failed”.
 
         return "Success";
+    }
+
+    public static void main(String[] args) { // Moved inside the class and fixed indentation
+        Person person1 = new Person("56s_d%&fAB", "John", "Citizen", "123|Example St|Melbourne|Victoria|Australia", "15-11-1990");
+        if (person1.addPerson()) {
+            System.out.println("Person added successfully.");
+        } else {
+            System.out.println("Failed to add person.");
+        }
     }
 }
